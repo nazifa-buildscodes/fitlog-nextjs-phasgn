f366b77d2baee3ad0ad716a0a161305c5b822469
Nazifa Nowsin <nazifanowsin2021@gmail.com>
2026-09-25 02:35:38 +0600

    complete FitLog assignment

--- app/loading.js
+++ app/loading.js
@@ -2,7 +2,7 @@ import LoadingSpinner from "@/components/LoadingSpinner";
 
 export default function Loading() {
   return (
-    <main className="container-fit py-11">
+    <main className="container-fit py-10">
       <LoadingSpinner />
     </main>
   );
